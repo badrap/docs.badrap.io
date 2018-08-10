b8ae8a90b8ca93dcbd9c4fa50daa0a727155786a
Joachim Viide <jviide@iki.fi>
2018-08-10 13:24:10 +0300

    Switch vuepress config to .js from .yml

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,38 @@
+module.exports = {
+  title: "Badrap docs",
+  locales: {
+    "/": {
+      lang: "en-US",
+      description: "Badrap documentation site"
+    },
+    "/fi/": {
+      lang: "fi",
+      description: "Badrapin dokumentaatiosivusto"
+    }
+  },
+  themeConfig: {
+    logo: "/badrap-logo.png",
+    sidebar: "auto",
+    docsRepo: "badrap/docs.badrap.io",
+    docsDir: "docs",
+    editLinks: true,
+    nav: [
+      {
+        text: "badrap.io",
+        link: "https://badrap.io"
+      }
+    ],
+    locales: {
+      "/": {
+        selectText: "Languages",
+        label: "English",
+        editLinkText: "Edit this page on GitHub"
+      },
+      "/fi/": {
+        selectText: "Kielet",
+        label: "Suomi",
+        editLinkText: "Muokkaa tätä sivua GitHubissa"
+      }
+    }
+  }
+};
